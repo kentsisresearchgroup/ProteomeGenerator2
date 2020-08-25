a64a67e2fbc813ba7c07cade8156a2fa7f2588e7
Nathaniel Kwok <ndk2001@med.cornell.edu>
2020-08-25 19:05:36 -0400

    final code documentation in progress

--- genome_personalization_module.py
+++ genome_personalization_module.py
@@ -1,60 +1,46 @@
+# Initialize home/working/output directories
 WD = config['directories']['working_and_output_dir']
 workdir: WD
 PG2_HOME=config['directories']['PG2_installation_dir']
 TMP_DIR=config['directories']['optional']['temp_dir']
 
-
+# Initialize stock references
 STOCK_GENOME_FASTA=config['stock_references']['genome']['fasta']
 STOCK_GENOME_GTF=config['stock_references']['genome']['gtf']
 
-
+# Cohort/Organism info
 COHORT=config['input_files']['genome_personalization_module']['cohort_or_organism_name']
 
+
 input_file_format = config['input_files']['genome_personalization_module']['input_file_format']
+
 if 'vcf' in input_file_format:
     INPUT_VCFS = config['input_files']['genome_personalization_module']['vcf_inputs'].keys()
-    INPUT_VCF_GZ_FILES = []
-    ALL_INPUT_VCF_SAMPLES = []
-    input_vcf_aligned_fastas = []
     INPUT_VCF_GZ_FILES = [config['input_files']['genome_personalization_module']['vcf_inputs'][x]['vcf.gz_file'] for x in INPUT_VCFS]
-    for vcf in INPUT_VCFS:
-        for name in config['input_files']['genome_personalization_module']['vcf_inputs'][vcf]['sample_names']:
-            ALL_INPUT_VCF_SAMPLES.append(name)
-    input_vcf_aligned_fastas = [config['input_files']['genome_personalization_module']['vcf_inputs'][x]['aligned_genome']['fasta'] for x in INPUT_VCFS]
     
+    input_vcf_aligned_fastas = [config['input_files']['genome_personalization_module']['vcf_inputs'][x]['aligned_genome']['fasta'] for x in INPUT_VCFS]
     for fa in input_vcf_aligned_fastas:
         assert(fa == STOCK_GENOME_FASTA), "Error: At least one of your input vcfs is aligned to a different reference genome than the provided stock ref genome.\n\
-            Please use UCSC liftOver or CrossMap to realign each vcf's genome coordinates with the provided stock ref genome.\n\
-            In the future, this will be performed automatically."
+            Please use UCSC liftOver or CrossMap to realign each vcf's genome coordinates with the provided stock ref genome, or recreate them using the correct reference genome."
 
 
+# To run genome personalization, one of the following 3 conditions must be met:
+
+# 1) (If fresh PG2 run) either germline and/or somatic variants are being called
 calling_variants = config['user_defined_workflow']['genome_personalization_module']['variant_calling_submodule']['call_germline_variants_with_GATK4_HaplotypeCaller'] or config['user_defined_workflow']['genome_personalization_module']['variant_calling_submodule']['if_inputs_are_matched_tumor-normal_samples']['call_somatic_variants_with_GATK4_Mutect2']
-#calling_variants = False if input_file_format == 'vcf' else True # True if input is 'fastq' or 'bam'
+
+# 2) (If continuing/re-running a previous PG2 run) either germline and/or somatic variants HAVE been called by PG2, and the corresponding output filenames/directories have not been changed
 just_called_variants = config['user_defined_workflow']['genome_personalization_module']['variant_calling_submodule']['continuation']['just_finished_variant_calling']
 
+# 3) User supplies externally called VCF(s) as inputs 
 if not (calling_variants or just_called_variants): assert 'vcf' in input_file_format, "in order to run genome personalization, you must either call variants on WGS/WES input data, or supply a called VCF."
-if calling_variants:
-    TUMOR_SAMPLES=[]
-    #NORMAL_SAMPLES=[]
-    
-    sample_dict_list = [dict(config['input_files']['genome_personalization_module'][fmt+'_inputs']) for fmt in input_file_format]
-    for d in sample_dict_list:
-        for sample_name in d.keys():
-            if d[sample_name]['matched_sample_params']['is_matched_sample'] == False or d[sample_name]['matched_sample_params']['tumor_or_normal']=='tumor': TUMOR_SAMPLES.append(sample_name)
 
-    """
-    sample_dict = dict(config['input_files']['genome_personalization_module'][input_file_format+'_inputs'])
-    for sample_name in sample_dict.keys():
-        if sample_dict[sample_name]['matched_sample_params']['is_matched_sample'] == False or sample_dict[sample_name]['matched_sample_params']['tumor_or_normal']=='tumor': TUMOR_SAMPLES.append(sample_name)
-        #elif sample_dict[sample_name]['matched_sample_params']['tumor_or_normal']=='normal':
-        #    NORMAL_SAMPLES.append(sample_name)
-        #else: assert False, "This should never happen!"
-    """
 
 try:
     fa_index_file = open('{}.fai'.format(STOCK_GENOME_FASTA))
 except FileNotFoundError:
     print("Genome FASTA is not indexed! Please run 'samtools faidx <FASTA>'.")
+
 CHROMOSOMES = [x.split('\t')[0] for x in fa_index_file.readlines()]
 HAPLOTYPES = [1,2] if config['parameters']['genome_personalization_module']['variant_calling']['make_customRef_diploid'] else [1]
 
@@ -72,7 +58,6 @@ subworkflow WGS_variant_calling:
     workdir: WD
 
 # For each chromosome, patch the stock reference with variants contained in the (phased) VCF.
-# *we use the included dev branch of bcftools because the latest release (1.9) has a bug in bcftools consensus*
 if (not just_called_variants) and calling_variants:
     rule genome_01_CreateChrWiseCustomRef:
         input: vcf=WGS_variant_calling("out/{study_group}/variant_calling/{cohort}.{study_group}.variant_calling_finished.vcf.gz")
@@ -107,13 +92,15 @@ else:
         params: n="1", R="'rusage[mem=4]'", J="chr-wise_customRef", o="out/logs/chr-wise/{study_group}.{htype}^{chr}.out", eo="out/logs/chr-wise/{study_group}.{htype}^{chr}.err",study_grp=COHORT+'_{study_group}'
         shell: "samtools faidx {STOCK_GENOME_FASTA} {wildcards.chr} | bcftools consensus -s {params.study_grp} -H {wildcards.htype} -p {wildcards.htype}_ -c {output.chain} {input.merged_vcf} > {output.fasta}"
 
+# Reconstitute each newly individualized haplotype's complete sequence by merging all chromosomes of that haplotype
 rule genome_02a_MergeChrWiseCustomRef:
     input: expand("out/custom_ref/chr_split/{{study_group}}/{{cohort}}.h-{{htype}}^{chr}.fa",chr=CHROMOSOMES),expand("out/custom_ref/chr_split/{{study_group}}/{{cohort}}.h-{{htype}}^{chr}.chain",chr=CHROMOSOMES)
     output: fasta="out/custom_ref/{cohort}.{study_group}.H{htype}.fa",chain="out/custom_ref/{cohort}.{study_group}.H{htype}.chain"
     params: n="1", R="'rusage[mem=4]'", J="merge_customRef", o="out/logs/merge_customRef.out", eo="out/logs/merge_customRef.err"
     shell: "awk 1 out/custom_ref/chr_split/{wildcards.study_group}/{wildcards.cohort}.h-{wildcards.htype}^*.fa > {output.fasta}; awk 1 out/custom_ref/chr_split/{wildcards.study_group}/{wildcards.cohort}.h-{wildcards.htype}^*.chain > {output.chain}"
 
-# Adjust ref annotation coords
+# Adjust genome annotation coords from reference to custom genome
+# *** NOTE: this step incurs some data loss ***
 rule genome_02b_LiftoverAnnotationGTF:
     input: chain="out/custom_ref/{cohort}.{study_group}.H{htype}.chain", vcf_refGtf=STOCK_GENOME_GTF
     output: "out/custom_ref/{cohort}.{study_group}.H{htype}.gtf"
